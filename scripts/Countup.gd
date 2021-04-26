17368a386d150878dede8b795a691f17c83cf17d
AzariasB <azarias.boutin@gmail.com>
2021-04-26 22:25:46 +0200

    Fixed last second bug

--- scripts/Countup.gd
+++ scripts/Countup.gd
@@ -10,14 +10,14 @@ func _to_time():
 		return str(val) + "s"
 	
 	var seconds = val % 60
-	seconds = "" if seconds == 0 else str(seconds) + "s"
 	val = (val - seconds) / 60
+	seconds = "" if seconds == 0 else str(seconds) + "s"
 	if val < 60:
 		return  str(val) + "mn "  + seconds
 	
 	var minutes = val % 60
-	minutes = "" if minutes == 0 else str(minutes) + "mn"
 	val = (val - minutes) / 60
+	minutes = "" if minutes == 0 else str(minutes) + "mn"
 	return PoolStringArray([str(val) + "hr", minutes, seconds]).join(" ")
 
 func _process(delta):
